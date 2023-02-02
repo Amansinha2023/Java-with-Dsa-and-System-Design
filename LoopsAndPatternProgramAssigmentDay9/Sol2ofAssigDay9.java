cfeee5aa9eaf0e86dca3d876313db7fecf7929cb
Amansinha2023 <mansinha2258@gmail.com>
2023-02-02 13:32:40 +0530

    Added comment

--- LoopsAndPatternProgramAssigmentDay9/Sol2ofAssigDay9.java
+++ LoopsAndPatternProgramAssigmentDay9/Sol2ofAssigDay9.java
@@ -11,16 +11,16 @@ class Sol2ofAssigDay9{
 
  for(int i=1;i<=n;i++){
   for(int s=1;s<=n-i;s++){
-   System.out.print(" ");
+   System.out.print(" ");    //Print spaces
    
    }
   for(int j=1;j<=i;j++){
-   System.out.print("*");
+   System.out.print("*");    //Print the half trangle 
   }
 
-  for(int k=2;k<=i;k++){
-   
-   System.out.print("*");
+  for(int k=2;k<=i;k++){          
+    
+   System.out.print("*");     //Print the Other half of tralgle from i=2   
    
   }
    System.out.println();
