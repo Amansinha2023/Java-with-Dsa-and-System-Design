c70c25184d3d0e5b64130e8a5d4efc3a819653b7
Amansinha2023 <mansinha2258@gmail.com>
2023-02-07 14:03:53 +0530

    Solution of Assignment on String in Java Part 2 (Day 15)

---
+++ StringInJavaPart2AssignmentDay15/Sol1ofAssigDay15.java
@@ -0,0 +1,11 @@
+//Progam to take string input from user.
+import java.util.*;
+class Sol1ofAssigDay15{
+ public static void main(String[] args) {
+  System.out.println("Input any string");
+  Scanner sc=new Scanner(System.in);
+  String s=sc.nextLine();
+  System.out.println(s);
+ }
+}
+
